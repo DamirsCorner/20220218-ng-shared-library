17e6775930575991dca5ac6038543211cabd2785
Damir Arh <damir.arh@gmail.com>
2022-02-07 09:27:58 +0100

    Include library in the application workspace

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { MyLibService } from 'my-lib';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'my-app';
+
+  constructor(myLibService: MyLibService) {}
 }
