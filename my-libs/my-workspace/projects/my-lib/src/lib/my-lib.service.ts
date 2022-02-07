832b16b9af792d68060b54fa52ba1d308043655b
Damir Arh <damir.arh@gmail.com>
2022-02-07 09:28:22 +0100

    Fix for peer dependencies

--- my-libs/my-workspace/projects/my-lib/src/lib/my-lib.service.ts
+++ my-libs/my-workspace/projects/my-lib/src/lib/my-lib.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MyLibService {
-
-  constructor() { }
+  constructor(http: HttpClient) {}
 }
